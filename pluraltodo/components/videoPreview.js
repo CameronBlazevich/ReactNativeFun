690bc8b1127c8c23c067499efcb40c116c131362
Cameron Blazevich <cameron.blazevich@gmail.com>
2017-07-24 21:25:33 -0500

    load youtube videos into thumbnails on video screen

--- pluraltodo/components/videoPreview.js
+++ pluraltodo/components/videoPreview.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
-import * as dougPolkWinnerPhoto from '../img/Doug_Polk_Winner_Photo.jpg';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
   preview: {
-    width: 125,
-    height: 100,
+    width: 160,
+    height: 120,
     margin: 5,
+  },
+  video: {
+    height: 450,
+    width: 500,
+  },
+  title: {
+    textAlign: 'center',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: 165,
+    height: 125,
   },
 });
 
-function VideoPreview() {
-  return <Image style={styles.preview} source={dougPolkWinnerPhoto.default} />;
+function VideoPreview({ video, onPressVideo }) {
+  // console.log(`video: ${JSON.stringify(video)}`);
+  return (
+    <TouchableOpacity onPress={() => onPressVideo(video.id)}>
+      <View style={styles.container}>
+        <Image
+          source={{ uri: video.thumbnailUrl }}
+          style={styles.preview}
+          resizeMode={Image.resizeMode.cover}
+        />
+        <Text style={styles.title}>
+          {video.title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 export default VideoPreview;
+
+VideoPreview.propTypes = {
+  video: PropTypes.shape({
+    thumbnail: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+  onPressVideo: PropTypes.func.isRequired,
+};
--- pluraltodo/components/videosScreen.js
+++ pluraltodo/components/videosScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, Button, StyleSheet, View, ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
+import { DOMParser } from 'xmldom';
 import HeadingRow from './headingRow';
 import VideoPreview from './videoPreview';
 import * as playIcon from '../icons/play-icon.png';
@@ -27,34 +28,66 @@ class VideoScreen extends React.Component {
     tabBarIcon: ({ tintColor }) =>
       <Image source={playIcon.default} style={[styles.icon, { tintColor }]} />,
   };
+  constructor(props) {
+    super(props);
+
+    this.state = this.getInitialState();
+  }
+
+  getInitialState() {
+    console.log('getting initial state');
+    return {
+      playlistId: 'PLFZTVU_cpCnubbV-pY4f7wg13rEzXbAeO',
+      videos: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchVideos();
+  }
+  // GET RID OF THESE PROMISES AND USE ASYNC AWAIT
+  fetchVideos() {
+    console.log('Fetching video feed...');
+    const url = `${'https://www.youtube.com/feeds/videos.xml?playlist_id='}${this.state
+      .playlistId}`;
+    fetch(url)
+      .then(response => response.text())
+      .then((responseText) => {
+        this.parseVideos(responseText);
+      })
+      .catch((error) => {
+        console.log('Error fetching the feed: ', error);
+      });
+  }
+
+  parseVideos(responseText) {
+    console.log(`Got feed with length: ${responseText.length}`);
+    const doc = new DOMParser().parseFromString(responseText, 'text/xml');
+    const objs = [];
+    const videos = doc.getElementsByTagName('yt:videoId');
+    const thumbs = doc.getElementsByTagName('media:thumbnail');
+    const titles = doc.getElementsByTagName('media:title');
+    // console.log(JSON.stringify(titles[0]));
+    // console.log(titles);
+    for (let i = 0; i < videos.length; i++) {
+      objs.push({
+        id: videos[i].textContent,
+        thumbnailUrl: thumbs[i].getAttribute('url'),
+        thumbnailWidth: thumbs[i].getAttribute('width'),
+        thumbnailHeight: thumbs[i].getAttribute('height'),
+        title: titles[i].textContent,
+      });
+    }
+    this.setState({ videos: objs });
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.body}>
           <HeadingRow text="Doug's Videos" />
-          <ScrollView contentContainerStyle={styles.contentContainer} horizontal>
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-          </ScrollView>
-          <HeadingRow text="Joey's Videos" />
-          <ScrollView contentContainerStyle={styles.contentContainer} horizontal>
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-          </ScrollView>
-          <HeadingRow text="Ryan's Videos" />
-          <ScrollView contentContainerStyle={styles.contentContainer} horizontal>
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
-            <VideoPreview />
+          <ScrollView horizontal>
+            {this.state.videos.map(video => <VideoPreview video={video} />)}
           </ScrollView>
         </View>
         <Button
@@ -70,4 +103,5 @@ export default VideoScreen;
 
 VideoScreen.propTypes = {
   navigation: PropTypes.shape({ navigate: PropTypes.func.isRequired }).isRequired,
+  // playlistId: PropTypes.string.isRequired,
 };
